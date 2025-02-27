e42aee3d87d77e567e232c4728b00f3c2523f1e0
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2025-02-27 12:07:01 +0100

    add debounce to inputs
    to make app feel more responsive

--- server.R
+++ server.R
@@ -38,31 +38,23 @@ limits <- db |> group_by(disease) |> summarize(
 
 print("Ready.")
 
+DEBOUNCE_MS <- 500
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  output$spark <- renderPlot({
+  sparkdata <- reactive({
     db |> filter(disease == local(input$disease)) |>
       group_by(year) |>
-      summarize(mentions = mean(normalized_mentions, na.rm = TRUE)) |>
-      ggplot(aes(x = year, y = mentions)) +
-      labs(y = "", x = "") +
-      scale_x_continuous(breaks = c(1830, 1850, 1870, 1890, 1910, 1930)) +
-      geom_line(colour = "darkgrey") +
-      geom_vline(xintercept = local(input$year), colour = "#08306b") +
-      theme_minimal() +
-      theme(
-        #axis.text.x = element_text(angle = 90),
-        panel.grid = element_blank(),
-        axis.ticks = element_blank(),
-        axis.line = element_blank(),
-        axis.text = element_blank(),
-        plot.margin = unit(c(0, 0, 0, -.1), "npc")
-      )
-  }, res = 90)
+      summarize(mentions = mean(normalized_mentions, na.rm = TRUE))
+  }) |>
+    debounce(DEBOUNCE_MS)
   
-  output$map <- renderPlot({
-    if (local(input$monthly)) {
+  sparkyear <- reactive({
+    input$year
+  }) |> debounce(DEBOUNCE_MS)
+  
+  mapdata <- reactive({
+    if (input$monthly) {
       dat <- db |>
         filter(
           year == local(input$year),
@@ -76,37 +68,61 @@ function(input, output, session) {
         )) |> collect()
     } else {
       dat <- db |>
-        filter(year == local(input$year),
-               disease == local(input$disease)) |>
+        filter(year == local(input$year), disease == local(input$disease)) |>
         group_by(cbscode) |>
         summarize(across(
           c(normalized_mentions, lower, upper),
           \(x) mean(x, na.rm = TRUE)
         )) |> collect()
     }
-    
-    titletext <- if (local(input$monthly)) {
+    map |> left_join(dat, by = "cbscode")
+  }) |> debounce(DEBOUNCE_MS)
+  
+  titletext <- reactive({
+    if (input$monthly) {
       glue(
         "{str_to_title(input$disease)} mentions in {month(input$month, label = TRUE)} {input$year}"
       )
     } else {
       glue("{str_to_title(input$disease)} mentions in {input$year}")
     }
-    
-    upperlimit <- limits |> filter(disease == local(input$disease)) |> pull(upper)
-    
-    map |>
-      left_join(dat, by = "cbscode") |>
-      ggplot(aes(fill = pmin(upperlimit, normalized_mentions))) +
+  }) |> debounce(DEBOUNCE_MS)
+  
+  upperlimit <- reactive({
+    limits |> filter(disease == local(input$disease)) |> pull(upper)
+  }) |> debounce(DEBOUNCE_MS)
+  
+  output$spark <- renderPlot({
+    sparkdata() |>
+      ggplot(aes(x = year, y = mentions)) +
+      labs(y = "", x = "") +
+      scale_x_continuous(breaks = c(1830, 1850, 1870, 1890, 1910, 1930)) +
+      geom_line(colour = "darkgrey") +
+      geom_vline(xintercept = sparkyear(), colour = "#08306b") +
+      theme_minimal() +
+      theme(
+        #axis.text.x = element_text(angle = 90),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        plot.margin = unit(c(0, 0, 0, -.1), "npc")
+      )
+  }, res = 90)
+  
+  output$map <- renderPlot({
+    mapdata() |>
+      ggplot(aes(fill = pmin(upperlimit(), normalized_mentions))) +
       scale_fill_gradient(
         na.value = "#ffffcc",
         low = "#f7fbff",
         high = "#08306b",
-        limits = c(0, upperlimit),
-        transform = scales::transform_pseudo_log(sigma = upperlimit/10)
+        limits = c(0, upperlimit()),
+        transform = scales::transform_pseudo_log(sigma = upperlimit() /
+                                                   10)
       ) +
       geom_sf(color = "transparent", size = 0.3) +
       theme_minimal() +
-      labs(fill = "Mention rate", title = titletext)
+      labs(fill = "Mention rate", title = titletext())
   }, res = 100)
 }
